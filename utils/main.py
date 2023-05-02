a0a01267c1c56bec1deabd146ec97f92b4f5f241
DimaSimakov <dimarodeo@gmail.com>
2023-05-02 22:15:00 +0700

    received origin data from json file

--- utils/main.py
+++ utils/main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+from function import import_data
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Имя файла с исходными данными
+file_input = "utils/operations.json"
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(import_data(file_input))
