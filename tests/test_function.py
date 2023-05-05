048347d7d986deca27fd3bc0f76bdb420ff89d79
DimaSimakov <dimarodeo@gmail.com>
2023-05-06 01:58:36 +0700

    tests cover running

--- tests/test_function.py
+++ tests/test_function.py
@@ -1,11 +1,14 @@
 import unittest
 from utils import function
 
-if __name__ == '__function__':
-    unittest.function()
+if __name__ == '__main__':
+    unittest.main()
+
 
 class TestFunction(unittest.TestCase):
 
     def test_print_date(self):
-
         self.assertEqual(function.print_date({'date': '2019-08-26T10:50:58.294041'}), '26.08.2019')
+        self.assertEqual(function.print_date({'date': '2019-07-16T10:50:58.294041'}), '16.07.2019')
+        self.assertEqual(function.print_date({'date': '2018-01-05T10:50:58.294041'}), '05.01.2018')
+        self.assertEqual(function.print_date({'date': '2017-01-05T10:50:58.294041'}), '05.01.2017')
