8b531f7ab46fd3d47682a7ce7db49c808699fb72
DimaSimakov <dimarodeo@gmail.com>
2023-05-05 18:19:14 +0700

    added code for print info in main file

--- utils/main.py
+++ utils/main.py
@@ -1,4 +1,5 @@
-from function import import_data, data_for_work, sorted_data
+from function import import_data, data_for_work, sorted_data, print_date
+from function import print_description, print_from, print_to, print_amount, print_currency
 
 # Имя файла с исходными данными
 file_input = "operations.json"
@@ -13,7 +14,14 @@ data_start = data_for_work(data_input)
 # сортируем список операций по дате и берем 5 последних операций
 data_out = sorted_data(data_start)
 
-#
-# вывод 1 строки в формате "дата" "назначение операции"
-# вывод второй строки в формате "откуда" -> "куда"
-# вывод третьей строки в формате "сумма" "валюта"
+# вывод информации по операциям
+for item in range(4, 0, -1):
+
+    # вывод 1 строки в формате "дата" "назначение операции"
+    print(f"{print_date(item)} {print_description(item)}")
+
+    # вывод второй строки в формате "откуда" -> "куда"
+    print(f"{print_from(item)}-> {print_to(item)}")
+
+    # вывод третьей строки в формате "сумма" "валюта"
+    print(f"{print_amount(item)} {print_currency(item)}")
