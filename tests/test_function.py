548c0626857fed2a6c1757ea989c893ccddbb48a
DimaSimakov <dimarodeo@gmail.com>
2023-05-05 20:33:31 +0700

    added test #1

--- tests/test_function.py
+++ tests/test_function.py
@@ -1,3 +1,11 @@
 import unittest
 from utils import function
 
+if __name__ == '__function__':
+    unittest.function()
+
+class TestArrs(unittest.TestCase):
+
+    def test_print_date(self):
+
+        self.assertEqual(function.print_date({'date': '2019-08-26T10:50:58.294041'}), '26.08.2019')
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@ from utils import main
 import unittest
 
 
-class TestArrs(unittest.TestCase):
-
-    def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+# class TestArrs(unittest.TestCase):
+#
+#     def test_get(self):
+#         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
