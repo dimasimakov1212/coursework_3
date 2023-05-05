049a704f6da4f779e7a5f42d395ccb8f0e0ac884
DimaSimakov <dimarodeo@gmail.com>
2023-05-05 18:03:36 +0700

    start code in main file

--- utils/main.py
+++ utils/main.py
@@ -1,9 +1,19 @@
-from function import import_data
+from function import import_data, data_for_work, sorted_data
 
 # Имя файла с исходными данными
 file_input = "operations.json"
 
 
+# загружаем данные из файла
+data_input = import_data(file_input)
+
+# предварительная обработка данных
+data_start = data_for_work(data_input)
+
+# сортируем список операций по дате и берем 5 последних операций
+data_out = sorted_data(data_start)
+
+#
 # вывод 1 строки в формате "дата" "назначение операции"
 # вывод второй строки в формате "откуда" -> "куда"
 # вывод третьей строки в формате "сумма" "валюта"
